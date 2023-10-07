e559ed66de4e291e56debfa6ccf058c84c6f56a7
Awardxz <jonkovaqi8@gmail.com>
2023-10-07 12:51:50 +0200

    INPUT SUPPORT

--- client.js
+++ client.js
@@ -1,27 +1,43 @@
-setInterval(() => {
-    const birthdate = new Date("2006-05-12");
-    const currentDate = new Date();
-    const oneSecond = 1000; // 1000 milliseconds in a second
-    const oneMinute = 60 * oneSecond; // 60 seconds in a minute
-    const oneHour = 60 * oneMinute; // 60 minutes in an hour
-    const oneDay = 24 * oneHour; // 24 hours in a day
+const inputDate = document.getElementById('date');
+let ageUpdateInterval; // Declare the interval variable outside the event listener
 
-    const totalMilliseconds = currentDate - birthdate;
-    const totalSeconds = Math.floor(totalMilliseconds / oneSecond);
-    const totalMinutes = Math.floor(totalMilliseconds / oneMinute);
-    const totalHours = Math.floor(totalMilliseconds / oneHour);
-    const totalDays = Math.floor(totalMilliseconds / oneDay);
+inputDate.addEventListener('input', () => {
+    clearInterval(ageUpdateInterval);
 
-    const ageInSeconds = totalSeconds % 60;
-    const ageInMinutes = totalMinutes % 60;
-    const ageInHours = totalHours % 24;
-    const ageInDays = totalDays;
+   
+    const birthdateString = inputDate.value;
+    const birthdate = new Date(birthdateString);
 
-    const agePercentage = (totalMilliseconds / (365.25 * 24 * 60 * 60 * 1000)); // Age percentage based on milliseconds
+    function updateAge() {
+        const currentDate = new Date();
+        const oneSecond = 1000; // 1000 milliseconds in a second
+        const oneMinute = 60 * oneSecond; // 60 seconds in a minute
+        const oneHour = 60 * oneMinute; // 60 minutes in an hour
+        const oneDay = 24 * oneHour; // 24 hours in a day
+    
+        const totalMilliseconds = currentDate - birthdate;
+        const totalSeconds = Math.floor(totalMilliseconds / oneSecond);
+        const totalMinutes = Math.floor(totalMilliseconds / oneMinute);
+        const totalHours = Math.floor(totalMilliseconds / oneHour);
+        const totalDays = Math.floor(totalMilliseconds / oneDay);
+    
+        const ageInSeconds = totalSeconds % 60;
+        const ageInMinutes = totalMinutes % 60;
+        const ageInHours = totalHours % 24;
+        const ageInDays = totalDays;
+    
+        const agePercentage = (totalMilliseconds / (365.25 * 24 * 60 * 60 * 1000) ); // Age percentage based on milliseconds
+    
+        const agePercentageWithDecimals = agePercentage
+    
+        document.getElementById('agePercentage').innerHTML = agePercentageWithDecimals.toFixed(8); // Display with eight decimal places
+    
+        console.log(`Your age is approximately ${ageInDays} days, ${ageInHours} hours, ${ageInMinutes} minutes, and ${ageInSeconds} seconds.`);
+    }
 
-    const agePercentageWithDecimals = agePercentage
+    // Initially update age
+    updateAge();
 
-    document.getElementById('agePercentage').innerHTML = agePercentageWithDecimals.toFixed(8); // Display with eight decimal places
-
-    console.log(`Your age is approximately ${ageInDays} days, ${ageInHours} hours, ${ageInMinutes} minutes, and ${ageInSeconds} seconds.`);
-}, 1); // blud updating every millisecond
+    // Start updating every 1 second
+    ageUpdateInterval = setInterval(updateAge, 1); // Assign the interval to the variable
+});
