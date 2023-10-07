86e97b60c4d7875ca063d9b55d412f71cb39b59d
Award <jonkovaqi2@gmail.com>
2023-10-07 17:20:46 +0200

    bismilah

--- client.js
+++ client.js
@@ -10,21 +10,8 @@ inputDate.addEventListener('input', () => {
 
     function updateAge() {
         const currentDate = new Date();
-        const oneSecond = 1000; // 1000 milliseconds in a second
-        const oneMinute = 60 * oneSecond; // 60 seconds in a minute
-        const oneHour = 60 * oneMinute; // 60 minutes in an hour
-        const oneDay = 24 * oneHour; // 24 hours in a day
-    
+
         const totalMilliseconds = currentDate - birthdate;
-        const totalSeconds = Math.floor(totalMilliseconds / oneSecond);
-        const totalMinutes = Math.floor(totalMilliseconds / oneMinute);
-        const totalHours = Math.floor(totalMilliseconds / oneHour);
-        const totalDays = Math.floor(totalMilliseconds / oneDay);
-    
-        const ageInSeconds = totalSeconds % 60;
-        const ageInMinutes = totalMinutes % 60;
-        const ageInHours = totalHours % 24;
-        const ageInDays = totalDays;
     
         const agePercentage = (totalMilliseconds / (365.25 * 24 * 60 * 60 * 1000) ); // Age percentage based on milliseconds
     
@@ -32,7 +19,7 @@ inputDate.addEventListener('input', () => {
     
         document.getElementById('agePercentage').innerHTML = agePercentageWithDecimals.toFixed(8); // Display with eight decimal places
     
-        console.log(`Your age is approximately ${ageInDays} days, ${ageInHours} hours, ${ageInMinutes} minutes, and ${ageInSeconds} seconds.`);
+        console.log(`Your age is approximately ${agePercentageWithDecimals}`);
     }
 
     // Initially update age
