af89b64d6efe37f3bd5b457a16f32230f8c02da3
Awardxz <jonkovaqi2@gmail.com>
2024-06-01 15:09:05 +0200

    'tiny changes - optimized'

--- client.js
+++ client.js
@@ -1,37 +1,19 @@
-const input = document.querySelector('.dateInput');
-console.log(input.value)
+const input = document.querySelector(".dateInput");
+console.log(input.value);
 let interval;
-input.addEventListener('input',()=> {
-    startTimer(input.value)
-})
+input.addEventListener("input", () => {
+  startTimer(input.value);
+});
 
 function startTimer(timer) {
-        clearInterval(interval)
-        interval =  setInterval(() => {
-        const birthdate = new Date(timer);
-        const currentDate = new Date();
-        const oneSecond = 1000; // 1000 milliseconds in a second
-        const oneMinute = 60 * oneSecond; // 60 seconds in a minute
-        const oneHour = 60 * oneMinute; // 60 minutes in an hour
-        const oneDay = 24 * oneHour; // 24 hours in a day
-    
-        const totalMilliseconds = currentDate - birthdate;
-        const totalSeconds = Math.floor(totalMilliseconds / oneSecond);
-        const totalMinutes = Math.floor(totalMilliseconds / oneMinute);
-        const totalHours = Math.floor(totalMilliseconds / oneHour);
-        const totalDays = Math.floor(totalMilliseconds / oneDay);
-    
-        const ageInSeconds = totalSeconds % 60;
-        const ageInMinutes = totalMinutes % 60;
-        const ageInHours = totalHours % 24;
-        const ageInDays = totalDays;
-    
-        const agePercentage = (totalMilliseconds / (365.25 * 24 * 60 * 60 * 1000)); // Age percentage based on milliseconds
-    
-        const agePercentageWithDecimals = agePercentage
-    
-        document.getElementById('agePercentage').innerHTML = agePercentageWithDecimals.toFixed(8); // Display with eight decimal places
-    
-    }, 1); // blud updating every millisecond
-    
+  clearInterval(interval);
+  interval = setInterval(() => {
+    const birthdate = new Date(timer);
+    const currentDate = new Date();
+    const totalMilliseconds = currentDate - birthdate;
+    const agePercentage = totalMilliseconds / (365.25 * 24 * 60 * 60 * 1000); // Age percentage based on milliseconds
+    const agePercentageWithDecimals = agePercentage;
+    document.getElementById("agePercentage").innerHTML =
+      agePercentageWithDecimals.toFixed(9); // Display with eight decimal places
+  }, 1); // blud updating every millisecond
 }
