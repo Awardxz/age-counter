f3acd6ae94bb556012a3c1459aa6087a284cd056
Awardxz <jonkovaqi2@gmail.com>
2024-06-07 00:00:21 +0200

    'fix'

--- client.js
+++ client.js
@@ -10,7 +10,7 @@ function startTimer(timer) {
   interval = setInterval(() => {
     const birthdate = new Date(timer);
     const currentDate = new Date();
-    const totalMilliseconds = currentDate - birthdate;
+    const totalMilliseconds = currentDate - birthdate - 1;
     const agePercentage = totalMilliseconds / (365.25 * 24 * 60 * 60 * 1000); // Age percentage based on milliseconds
     const agePercentageWithDecimals = agePercentage;
     document.getElementById("agePercentage").innerHTML =
